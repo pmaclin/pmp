0bde5fc41f61dacf5a0764c24d5372bf00bef67f
Maclin Phil <philmaclin@gmail.com>
2016-12-01 13:18:02 -0600

    Adds ability to Mark tasks as Complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_project
+  before_action :set_task, except: [:create]
 
 
   def create
@@ -8,13 +9,17 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = @project.tasks.find(params[:id])
     if @task.destroy
       flash[:success] = "Task was deleted! Yaay!"
     else
       flash[:error] = "Task was not deleted. Waaaah!"
     end
     redirect_to @project
+  end
+
+  def complete
+    @task.update_attribute(:completed_at, Time.now)
+    redirect_to @project, notice: "Task Completed!"
   end
 
 
@@ -24,6 +29,10 @@ class TasksController < ApplicationController
 
   def set_project
     @project = Project.find(params[:project_id])
+  end
+
+  def set_task
+    @task = @project.tasks.find(params[:id])
   end
 
   def task_params
